587e1c0fe5b9eb505aa414ab69250c77bad45167
Jay Maheshbhai Patel <jpatel4@isimtl.local>
2020-05-12 16:21:49 -0400

    Changed sign up and sign in design

--- php/register.php
+++ php/register.php
@@ -6,61 +6,41 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-	<link rel="stylesheet" type="text/css" href="main.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
 	
 <div class="container">
-    <div class="row">
-        <div class="col-md-12 min-vh-100 d-flex flex-column justify-content-center">
-            <div class="row">
-                <div class="col-lg-6 col-md-8 mx-auto">
 
-                    <!-- form card login -->
-                    <div class="card rounded shadow shadow-sm">
-                        <div class="card-header p-5 bg-dark" ">
-                            <h3 class="mb-0 text-center reg-col">Register</h3>
-                        </div>
-                        <div class="card-body">
-                            <form class="form" role="form" autocomplete="off" id="formLogin" novalidate="" method="POST">
-                                <div class="form-group">
-                                    <label for="uname1">Email</label>
-                                    <input type="email" class="form-control form-control-lg rounded-0" name="uname1" id="email" required="">
-                                    <div class="invalid-feedback">Oops, you missed this one.</div>
-                                </div>
-                                <div class="form-group">
-                                    <label>Password</label>
-                                    <input type="password" class="form-control form-control-lg rounded-0" id="pass" required="" autocomplete="new-password">
-                                    <div class="invalid-feedback">Enter your password too!</div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label>Confirm Password</label>
-                                    <input type="password" class="form-control form-control-lg rounded-0" id="conpass" required="" autocomplete="new-password">
-                                    <div class="invalid-feedback">Enter your confirm password too!</div>
-                                </div>
-                                <div class="text-center"> 
-                                    <button type="submit" class="btn btn-success btn-lg" id="btnSignup">SignUp</button>
-                                    <button type="submit" class="btn btn-danger btn-lg" id="btnLogin">Go to Login</button>
-                                </div
-                            </form>
-                        </div>
-                        <!--/card-block-->
-                    </div>
-                    <!-- /form card login -->
-
+    <div class="row center-div mt-5">
+        <div class="">
+            <h1 class="border border-primary rounded text-center">Activity tracker</h1>
+            <form class=" p-5 border border-primary rounded">
+                <h4 class="border border-primary text-center"> Sign up </h4>
+                
+                <div class="form-group">
+                <label for="email">Email</label>
+                <input type="text" class="form-control" id="email" placeholder="john@doe.com">
                 </div>
-
-
-            </div>
-            <!--/row-->
-
+                <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password">
+                </div>
+                <div class="form-group">
+                <label for="cpassword">Confirm password</label>
+                <input type="password" class="form-control" id="cpassword">
+                </div>
+                <div class="form-group text-center">
+                    <button type="submit" class="btn btn-primary">Sign up</button>
+                </div>
+                <div class="form-group text-center">
+                    Already a member? <a href="login.php">Sign in from here</a> 
+                </div>
+            </form>
         </div>
-        <!--/col-->
     </div>
     <!--/row-->
 </div>
 <!--/container-->
-	
 </body>
 </html>
