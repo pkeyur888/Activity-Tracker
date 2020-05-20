770f527c5400263611f3b625bb9fb53b17a71857
Keyurkumar Rameshbhai Patel <kpatel3@isimtl.local>
2020-05-20 18:25:03 -0400

    Edit and Delete Functionality

--- php/db/conn.php
+++ php/db/conn.php
@@ -2,7 +2,7 @@
 <?php
     global $conn;
   session_start();
-$conn = mysqli_connect("db:3306", "activitytrackeruser", "secret", "activitytrackerdb");
+$conn = mysqli_connect("localhost", "root", "", "activity_tracker");
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
   }
